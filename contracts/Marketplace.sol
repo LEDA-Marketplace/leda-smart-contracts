b5ca04665cd996dacd8f73d40f261d1529c8f198
Gimer Cervera <gimer.cervera@stackitgroup.com>
2022-10-05 13:14:15 -0500

    feat: adding creator royalties distribution and listing fees

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -9,7 +9,8 @@ import "hardhat/console.sol";
 
 contract Marketplace is Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
-    uint public feePercent; // the fee percentage on sales 
+    uint public feePercent; // the fee percentage on sales
+    uint public listingFeePercent;
     Counters.Counter public itemsCount;
     Counters.Counter public itemsSold;
 
@@ -26,6 +27,8 @@ contract Marketplace is Ownable, ReentrancyGuard {
         uint tokenId;
         uint price;
         address payable seller;
+        address payable creator;
+        uint creatorRoyaltiesPercent;
         item_status status;
     }
 
@@ -33,20 +36,21 @@ contract Marketplace is Ownable, ReentrancyGuard {
     mapping(uint => Item) public items;
 
     event LogCreateItem(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller
+        uint _itemId,
+        address indexed _nft,
+        uint _tokenId,
+        uint _price,
+        address indexed _seller,
+        address _creator
     );
 
     event LogBuyItem(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller,
-        address indexed buyer
+        uint _itemId,
+        address indexed _nft,
+        uint _tokenId,
+        uint _price,
+        address indexed _seller,
+        address indexed _buyer
     );
 
     event LogChangeStatus(
@@ -56,7 +60,14 @@ contract Marketplace is Ownable, ReentrancyGuard {
 
     constructor(uint _feePercent) {
         feePercent = _feePercent;
-        //feeAccount = payable(msg.sender);
+        listingFeePercent = 0;
+    }
+
+    function setListingFeesPercent(uint _listingFeePercent) 
+        onlyOwner 
+        external 
+    {
+        listingFeePercent = _listingFeePercent;
     }
 
     function setFeePercent(uint _feePercent) 
@@ -66,17 +77,32 @@ contract Marketplace is Ownable, ReentrancyGuard {
         feePercent = _feePercent;
     }
 
+    function getListingFees(uint _price)
+        private
+        view
+        returns(uint)
+    {
+        return (_price * listingFeePercent)/1000;
+    }
+
     // Make item to offer on the marketplace
-    function makeItem(address _nft, uint _tokenId, uint _price)
+    function makeItem(address _nft, uint _tokenId, uint _price, address _creator, uint _creatorRoyaltiesPercent)
         external
         nonReentrant
+        payable
     {
-        require(_price > 0, "Price must be greater than zero");
+        require(_price > 0, "Price must be greater than zero!");
+        uint listingFeesAmount = getListingFees(_price);
+        require(listingFeesAmount <= msg.value, "Should pay listing fees!");
         // increment itemCount
         itemsCount.increment();
         uint itemId = itemsCount.current();
         // transfer nft
         IERC721(_nft).transferFrom(msg.sender, address(this), _tokenId);
+
+        if(listingFeesAmount > 0)
+            payable(address(this)).transfer(listingFeesAmount);
+
         // add new item to items mapping
         items[itemId] = Item(
             itemId,
@@ -84,6 +110,8 @@ contract Marketplace is Ownable, ReentrancyGuard {
             _tokenId,
             _price,
             payable(msg.sender),
+            payable(_creator),
+            _creatorRoyaltiesPercent,
             item_status.Not_Listed
         );
         // emit Offered event
@@ -92,7 +120,8 @@ contract Marketplace is Ownable, ReentrancyGuard {
             _nft,
             _tokenId,
             _price,
-            msg.sender
+            msg.sender,
+            _creator
         );
     }
 
@@ -103,7 +132,6 @@ contract Marketplace is Ownable, ReentrancyGuard {
         require(item.seller == msg.sender, "only seller can change status!");
         require(item.status != _newStatus, "status should be new!");
         require(item.status != item_status.Sold, "item already sold!");
-
         item.status = _newStatus;
         emit LogChangeStatus(_itemId, msg.sender, _newStatus);
     }
@@ -130,15 +158,20 @@ contract Marketplace is Ownable, ReentrancyGuard {
         payable 
         nonReentrant 
     {
-        uint _totalPrice = getTotalPrice(_itemId);
         Item storage item = items[_itemId];
         require(_itemId > 0 && _itemId <= itemsCount.current(), "item does not exist");
-        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(msg.value >= item.price, "not enough ether to cover item price and market fee");
         require(item.status != item_status.Sold, "item already sold");
         require(item.status == item_status.Listed, "item should be listed");
+
+        (uint platformFees, uint sellerAmount, uint creatorAmount) 
+            = getRoyalties(_itemId, item.creatorRoyaltiesPercent);
+
         // pay seller and feeAccount
-        item.seller.transfer(item.price);
-        //feeAccount.transfer(_totalPrice - item.price);
+        item.creator.transfer(creatorAmount);
+        item.seller.transfer(sellerAmount);
+        payable(address(this)).transfer(platformFees);
+        
         // update item to sold
         item.status =  item_status.Sold;
         // transfer nft to buyer
@@ -173,11 +206,15 @@ contract Marketplace is Ownable, ReentrancyGuard {
         return itemsCount.current();
     }
 
-    function getTotalPrice(uint _itemId) 
+    function getRoyalties(uint _itemId, uint _creatorRoyaltiesPercent) 
         view 
         public 
-        returns(uint)
+        returns(uint _platformFees, uint _sellerAmount, uint _creatorAmount)
     {
-        return((items[_itemId].price*(100 + feePercent))/100);
+        uint _price = items[_itemId].price;
+        
+        _creatorAmount = (_price * _creatorRoyaltiesPercent)/1000; 
+        _platformFees = (_price * feePercent)/1000;
+        _sellerAmount = _price - _platformFees - _creatorAmount;
     }
 }
